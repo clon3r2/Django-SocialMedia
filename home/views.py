04c07f4c70752051b5153f489aedd6cfcf532dc8
Alireza <alirezac257@gmail.com>
2022-02-13 01:35:33 +0330

    added Post Detail View

--- home/views.py
+++ home/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from .models import Post
 
 class HomeView(View):
     template_name = 'home/index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        posts = Post.objects.all()
+        return render(request, self.template_name, {'posts': posts})
 
     def post(self, request):
         pass
+
+
+class PostDetailView(View):
+    template_name = 'home/post.html'
+
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk=post_id, slug=post_slug)
+        return render(request, self.template_name, {'post': post})
