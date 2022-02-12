1f20fd2977f0d6c3bcdb161233247aa76673ca97
Alireza <alirezac257@gmail.com>
2022-02-13 01:34:47 +0330

    post detail view url added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='index')
+    path('', views.HomeView.as_view(), name='index'),
+    path('post/<int:post_id>/<slug:post_slug>', views.PostDetailView.as_view(), name='post'),
 ]
