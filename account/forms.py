6ff49dd24dfed70dff7374c9751f8acf0532f755
Alireza <alirezac257@gmail.com>
2022-02-12 22:05:24 +0330

    register form validations added

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,33 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError('This username already exists !')
+        else:
+            return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('This email already exists !')
+        else:
+            return email
+
+    def clean(self):
+        clean_data = super().clean()
+        password = clean_data.get('password1')
+        confirm = clean_data.get('password2')
+
+        if password and confirm and password != confirm:
+            raise ValidationError('Password is not match !')
+
