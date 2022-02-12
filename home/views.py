877f05534c5b1e6b8a7e0231ea4ccd848add4bbf
Alireza <alirezac257@gmail.com>
2022-02-13 02:08:41 +0330

    Delete post view added

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views import View
 from .models import Post
 
@@ -20,3 +21,15 @@ class PostDetailView(View):
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
         return render(request, self.template_name, {'post': post})
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        if request.user.id == post.user.id:
+            post.delete()
+            messages.success(request, 'post deleted successfully', 'success')
+        else:
+            messages.error(request, 'you cant delete this post', 'danger')
+        return redirect('home:index')
+
