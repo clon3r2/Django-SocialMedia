81c34ae2443739a4ecc1cc9c4bb460a300478dd6
Alireza <alirezac257@gmail.com>
2022-02-12 22:38:36 +0330

    added Login View

--- account/views.py
+++ account/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
-class RegisterView(View):
+class UserRegisterView(View):
     template_name = 'account/register.html'
 
     def get(self, request):
@@ -22,3 +23,24 @@ class RegisterView(View):
             return redirect('home:index')
         else:
             return render(request, self.template_name, {'form': form})
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            clean_data = form.cleaned_data
+            user = authenticate(request, username=clean_data['username'], password=clean_data['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('home:index')
+            else:
+                messages.success(request, 'username or password is not correct', 'warning')
+        return render(request, self.template_name, {'form': form})
