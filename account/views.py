492e67ed3fc86fa120f3339008084c39eb91f25a
Alireza <alirezac257@gmail.com>
2022-02-12 23:30:41 +0330

    url protecting with dispatch() for Login

--- account/views.py
+++ account/views.py
@@ -35,6 +35,12 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect('home:index')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         form = self.form_class
         return render(request, self.template_name, {'form': form})
