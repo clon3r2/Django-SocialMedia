5f830bf1f1afeaf2ae0cb7f5714e42d9ece7901d
Alireza <alirezac257@gmail.com>
2022-02-14 00:14:59 +0330

    created comments model

--- home/models.py
+++ home/models.py
@@ -20,3 +20,16 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created']
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='UserComments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='PostComments')
+    body = models.TextField()
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='ReplyComments', blank=True, null=True)
+    is_reply = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:30]}'
+
