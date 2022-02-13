fddc580d97887833adda0e27cdc5bfca68294e9a
Alireza <alirezac257@gmail.com>
2022-02-13 13:46:57 +0330

    added create post url

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:post_id>/<slug:post_slug>', views.PostDetailView.as_view(), name='post'),
     path('post/delete/<int:post_id>', views.PostDeleteView.as_view(), name='delete-post'),
     path('post/update/<int:post_id>', views.PostUpdateView.as_view(), name='update-post'),
+    path('post/create/', views.PostCreateView.as_view(), name='create-post'),
 ]
