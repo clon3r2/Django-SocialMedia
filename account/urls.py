4b1899947c23116765dea4d94ff811619f86852e
Alireza <alirezac257@gmail.com>
2022-02-13 16:16:59 +0330

    reset password urls added

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('profile/<int:user_id>', views.UserProfileView.as_view(), name='profile'),
-
+    path('reset/', views.UserPasswordResetView.as_view(), name='reset_password'),
+    path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
+
